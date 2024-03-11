375a41429446db1b901f05f68a2981d495197fd8
21pele <147058739+21pele@users.noreply.github.com>
2024-03-11 19:26:48 +0100

    Diseño mejorado de la pantalla de login

--- lib/log_in/log_in_screen.dart
+++ lib/log_in/log_in_screen.dart
@@ -19,74 +19,42 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Builder(
-              builder: (context) => IconButton(
-                    icon: const Icon(Icons.menu),
-                    color: Color.fromRGBO(0, 0, 0, 1),
-                    tooltip: 'Menu',
-                    onPressed: () => Scaffold.of(context).openDrawer(),
-                  )),
-          title: Text('Login Form'),
-        ),
-        drawer: Drawer(
-          backgroundColor: Color.fromRGBO(49, 45, 45, 1),
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle, //MIRAR QUE FORMA LE DOY
-                  image: DecorationImage(
-                    image: AssetImage("../assets/images/Logo.png"),
-                    fit: BoxFit.cover,
+        debugShowCheckedModeBanner: false,
+        home: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("../assets/images/board.jpg"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Color.fromRGBO(49, 45, 45, 1),
+                title: GestureDetector(
+                  onTap: () {
+                    GoRouter.of(context).push('/');
+                  },
+                  child: Text(
+                    'ChessHub',
+                    style: TextStyle(
+                        fontFamily: 'Oswald',
+                        color: Color.fromRGBO(255, 255, 255, 1)),
                   ),
                 ),
-                child: null,
               ),
-              ListTile(
-                title: Text(
-                  'Ajedrez Demo',
-                  style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+              body: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: LoginFormWidget(),
                 ),
-                onTap: () {
-                  GoRouter.of(context).go('/chess');
-                },
               ),
-              ListTile(
-                title: Text('Play',
-                    style: TextStyle(color: Color.fromRGBO(255, 136, 0, 1))),
-                onTap: () {
-                  GoRouter.of(context).go('/play');
-                },
-              ),
-              ListTile(
-                title: Text('Ranking',
-                    style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1))),
-                onTap: () {
-                  GoRouter.of(context).go('/ranking');
-                },
-              ),
-              ListTile(
-                title: Text('Settings',
-                    style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1))),
-                onTap: () {
-                  GoRouter.of(context).push('/settings');
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: LoginFormWidget(),
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        ));
   }
 }
 
@@ -106,28 +74,71 @@ class LoginFormWidgetState extends State<LoginFormWidget> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        TextField(
-          controller: _usernameController,
-          decoration: InputDecoration(
-            labelText: 'Username',
+        Container(
+            color: Color.fromRGBO(49, 45, 45, 1),
+            width: 350,
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Text('Log In',
+                        style:
+                            TextStyle(color: Color.fromRGBO(255, 136, 0, 1)))),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: TextField(
+                    expands: false,
+                    cursorColor: Color.fromRGBO(255, 136, 0, 1),
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      floatingLabelStyle:
+                          TextStyle(color: Color.fromRGBO(255, 136, 0, 1)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(255, 136, 0,
+                                1)), // Color de resaltado al hacer clic
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: TextField(
+                    cursorColor: Color.fromRGBO(255, 136, 0, 1),
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      floatingLabelStyle:
+                          TextStyle(color: Color.fromRGBO(255, 136, 0, 1)),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(255, 136, 0,
+                                1)), // Color de resaltado al hacer clic
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: ElevatedButton(
+            onPressed: () {
+              settingsController
+                  .toggleLoggedIn(); //AQUI HABRIA QUE PASARLE AL BACKEND LOS DATOS Y CONSULTAR EL USUARIO Y CONTRASEÑA
+              GoRouter.of(context).go('/');
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Color.fromRGBO(255, 136, 0, 1)),
+            ),
+            child: Text('Login',
+                style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
           ),
-        ),
-        SizedBox(height: 20.0),
-        TextField(
-          controller: _passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            labelText: 'Password',
-          ),
-        ),
-        SizedBox(height: 20.0),
-        ElevatedButton(
-          onPressed: () {
-            settingsController
-                .toggleLoggedIn(); //AQUI HABRIA QUE PASARLE AL BACKEND LOS DATOS Y CONSULTAR EL USUARIO Y CONTRASEÑA
-            GoRouter.of(context).go('/');
-          },
-          child: Text('Login'),
         ),
       ],
     );
