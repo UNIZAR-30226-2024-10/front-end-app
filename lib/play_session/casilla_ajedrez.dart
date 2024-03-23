f59b01d2abfa58a5e495c9f0b92b83b082c24778
21pele <147058739+21pele@users.noreply.github.com>
2024-03-23 13:09:01 +0100

    Seleccionar las fichas funciona, elliminado un fichero de log in innecesario

--- lib/play_session/casilla_ajedrez.dart
+++ lib/play_session/casilla_ajedrez.dart
@@ -8,25 +8,42 @@ import 'package:flutter_svg/flutter_svg.dart';
 class CasillaAjedrez extends StatelessWidget {
   final PiezaAjedrez? pieza;
   final bool esBlanca;
+  final bool seleccionada;
+  final void Function()? onTap;
 
   const CasillaAjedrez({
     Key? key,
     this.pieza,
     required this.esBlanca,
+    required this.seleccionada,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color:esBlanca ? Color(0xFFADF597) : Color(0XFF2E960F),
-      child: pieza != null ? 
-      //adaptar la imagen de la pieza
-      SvgPicture.asset(
-              pieza!.nombreImagen,
-              width: 24, // Ajusta el tamaño según sea necesario
-              height: 24,
-            )
-          : null,
+    Color? colorCasilla;
+
+    if (seleccionada) {
+      colorCasilla = const Color.fromARGB(255, 223, 85, 75);
+    } else {
+      colorCasilla =
+          colorCasilla = esBlanca ? Color(0xFFADF597) : Color(0XFF2E960F);
+    }
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: colorCasilla,
+        child: pieza != null
+            ?
+            //adaptar la imagen de la pieza
+            SvgPicture.asset(
+                pieza!.nombreImagen,
+                width: 24, // Ajusta el tamaño según sea necesario
+                height: 24,
+              )
+            : null,
+      ),
     );
   }
 }
--- lib/play_session/tablero_widget.dart
+++ lib/play_session/tablero_widget.dart
@@ -22,157 +22,160 @@ class TableroAjedrez extends StatefulWidget {
   State<TableroAjedrez> createState() => _TableroAjedrezState();
 }
 
-class _TableroAjedrezState extends State<TableroAjedrez>{
+class _TableroAjedrezState extends State<TableroAjedrez> {
   late List<List<PiezaAjedrez?>> tablero;
 
+  PiezaAjedrez? piezaSeleccionada;
+
+  int filaSeleccionada = -1;
+
+  int columnaSeleccionada = -1;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _inicializarTablero();
   }
 
-  void _inicializarTablero(){
-    List<List<PiezaAjedrez?>> nuevoTablero = List.generate(8, (index) => List.generate(8, (index) => null));
-    
+  void _inicializarTablero() {
+    List<List<PiezaAjedrez?>> nuevoTablero =
+        List.generate(8, (index) => List.generate(8, (index) => null));
+
     //Place pawn
-    for(int i = 0; i < 8; i++){
+    for (int i = 0; i < 8; i++) {
       nuevoTablero[1][i] = PiezaAjedrez(
-        tipoPieza: TipoPieza.PEONES,
-        esBlanca: false,
-        nombreImagen: 'assets/images/pawn-b.svg'
-      );
+          tipoPieza: TipoPieza.PEONES,
+          esBlanca: false,
+          nombreImagen: 'assets/images/pawn-b.svg');
 
       nuevoTablero[6][i] = PiezaAjedrez(
-        tipoPieza: TipoPieza.PEONES,
-        esBlanca: true,
-        nombreImagen: 'assets/images/pawn-w.svg'
-      );
+          tipoPieza: TipoPieza.PEONES,
+          esBlanca: true,
+          nombreImagen: 'assets/images/pawn-w.svg');
     }
 
     //Place rooks
     nuevoTablero[0][0] = PiezaAjedrez(
-      tipoPieza: TipoPieza.TORRES,
-      esBlanca: false,
-      nombreImagen: 'assets/images/rook-b.svg'
-    );
+        tipoPieza: TipoPieza.TORRES,
+        esBlanca: false,
+        nombreImagen: 'assets/images/rook-b.svg');
     nuevoTablero[0][7] = PiezaAjedrez(
-      tipoPieza: TipoPieza.TORRES,
-      esBlanca: false,
-      nombreImagen: 'assets/images/rook-b.svg'
-    );
+        tipoPieza: TipoPieza.TORRES,
+        esBlanca: false,
+        nombreImagen: 'assets/images/rook-b.svg');
     nuevoTablero[7][0] = PiezaAjedrez(
-      tipoPieza: TipoPieza.TORRES,
-      esBlanca: true,
-      nombreImagen: 'assets/images/rook-w.svg'
-    );
+        tipoPieza: TipoPieza.TORRES,
+        esBlanca: true,
+        nombreImagen: 'assets/images/rook-w.svg');
     nuevoTablero[7][7] = PiezaAjedrez(
-      tipoPieza: TipoPieza.TORRES,
-      esBlanca: true,
-      nombreImagen: 'assets/images/rook-w.svg'
-    );
+        tipoPieza: TipoPieza.TORRES,
+        esBlanca: true,
+        nombreImagen: 'assets/images/rook-w.svg');
 
     //Place knights
     nuevoTablero[0][1] = PiezaAjedrez(
-      tipoPieza: TipoPieza.CABALLOS,
-      esBlanca: false,
-      nombreImagen: 'assets/images/knight-b.svg'
-    );
+        tipoPieza: TipoPieza.CABALLOS,
+        esBlanca: false,
+        nombreImagen: 'assets/images/knight-b.svg');
     nuevoTablero[0][6] = PiezaAjedrez(
-      tipoPieza: TipoPieza.CABALLOS,
-      esBlanca: false,
-      nombreImagen: 'assets/images/knight-b.svg'
-    );
+        tipoPieza: TipoPieza.CABALLOS,
+        esBlanca: false,
+        nombreImagen: 'assets/images/knight-b.svg');
     nuevoTablero[7][1] = PiezaAjedrez(
-      tipoPieza: TipoPieza.CABALLOS,
-      esBlanca: true,
-      nombreImagen: 'assets/images/knight-w.svg'
-    );
+        tipoPieza: TipoPieza.CABALLOS,
+        esBlanca: true,
+        nombreImagen: 'assets/images/knight-w.svg');
     nuevoTablero[7][6] = PiezaAjedrez(
-      tipoPieza: TipoPieza.CABALLOS,
-      esBlanca: true,
-      nombreImagen: 'assets/images/knight-w.svg'
-    );
+        tipoPieza: TipoPieza.CABALLOS,
+        esBlanca: true,
+        nombreImagen: 'assets/images/knight-w.svg');
 
     //Place bishops
     nuevoTablero[0][2] = PiezaAjedrez(
-      tipoPieza: TipoPieza.ALFILES,
-      esBlanca: false,
-      nombreImagen: 'assets/images/bishop-b.svg'
-    );
+        tipoPieza: TipoPieza.ALFILES,
+        esBlanca: false,
+        nombreImagen: 'assets/images/bishop-b.svg');
     nuevoTablero[0][5] = PiezaAjedrez(
-      tipoPieza: TipoPieza.ALFILES,
-      esBlanca: false,
-      nombreImagen: 'assets/images/bishop-b.svg'
-    );
+        tipoPieza: TipoPieza.ALFILES,
+        esBlanca: false,
+        nombreImagen: 'assets/images/bishop-b.svg');
     nuevoTablero[7][2] = PiezaAjedrez(
-      tipoPieza: TipoPieza.ALFILES,
-      esBlanca: true,
-      nombreImagen: 'assets/images/bishop-w.svg'
-    );
+        tipoPieza: TipoPieza.ALFILES,
+        esBlanca: true,
+        nombreImagen: 'assets/images/bishop-w.svg');
     nuevoTablero[7][5] = PiezaAjedrez(
-      tipoPieza: TipoPieza.ALFILES,
-      esBlanca: true,
-      nombreImagen: 'assets/images/bishop-w.svg'
-    );
+        tipoPieza: TipoPieza.ALFILES,
+        esBlanca: true,
+        nombreImagen: 'assets/images/bishop-w.svg');
 
     //Place queens
     nuevoTablero[0][3] = PiezaAjedrez(
-      tipoPieza: TipoPieza.REINAS,
-      esBlanca: false,
-      nombreImagen: 'assets/images/queen-b.svg'
-    );
+        tipoPieza: TipoPieza.REINAS,
+        esBlanca: false,
+        nombreImagen: 'assets/images/queen-b.svg');
     nuevoTablero[7][4] = PiezaAjedrez(
-      tipoPieza: TipoPieza.REINAS,
-      esBlanca: true,
-      nombreImagen: 'assets/images/queen-w.svg'
-    );
+        tipoPieza: TipoPieza.REINAS,
+        esBlanca: true,
+        nombreImagen: 'assets/images/queen-w.svg');
     //Place kings
     nuevoTablero[0][4] = PiezaAjedrez(
-      tipoPieza: TipoPieza.REYES,
-      esBlanca: false,
-      nombreImagen: 'assets/images/king-b.svg'
-    );
+        tipoPieza: TipoPieza.REYES,
+        esBlanca: false,
+        nombreImagen: 'assets/images/king-b.svg');
     nuevoTablero[7][3] = PiezaAjedrez(
-      tipoPieza: TipoPieza.REYES,
-      esBlanca: true,
-      nombreImagen: 'assets/images/king-w.svg'
-    );
+        tipoPieza: TipoPieza.REYES,
+        esBlanca: true,
+        nombreImagen: 'assets/images/king-w.svg');
 
     tablero = nuevoTablero;
-  } 
-  
+  }
+
+  void seleccionadaPieza(int fila, int columna) {
+    setState(() {
+      if (tablero[fila][columna] != null) {
+        piezaSeleccionada = tablero[fila][columna];
+        filaSeleccionada = fila;
+        columnaSeleccionada = columna;
+      }
+    });
+  }
+
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Column(
-      children: [
-        Expanded(
-          child: GridView.builder(
-            itemCount: 8 * 8,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
-            itemBuilder: (context, index) {
-              int fila = index ~/ 8;
-              int columna = index % 8;
-              return CasillaAjedrez(
-                esBlanca: esBlanca(index),
-                pieza: tablero[fila][columna], 
-              );
-            },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              itemCount: 8 * 8,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 8),
+              itemBuilder: (context, index) {
+                int fila = index ~/ 8;
+                int columna = index % 8;
+
+                bool seleccionada =
+                    filaSeleccionada == fila && columnaSeleccionada == columna;
+
+                return CasillaAjedrez(
+                  seleccionada: seleccionada,
+                  esBlanca: esBlanca(index),
+                  pieza: tablero[fila][columna],
+                  onTap: () => seleccionadaPieza(fila, columna),
+                );
+              },
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            onPressed: () => GoRouter.of(context).go('/'),
-            child: const Text('Back'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () => GoRouter.of(context).go('/'),
+              child: const Text('Back'),
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
-}
-
-
-
