3f61e79e406620a121f87091a0a2c457fc7dd2a5
21pele <147058739+21pele@users.noreply.github.com>
2024-04-17 15:45:36 +0200

    Estructura pantalla personalización mejorada
    Aún no esta terminada pero la estructura ya está

--- lib/level_selection/personalizacion.dart
+++ lib/level_selection/personalizacion.dart
@@ -3,93 +3,144 @@ import 'package:go_router/go_router.dart';
 import '../style/my_button.dart';
 
 class Personalizacion extends StatelessWidget {
+  bool _isSwitched = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Personalización'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para el primer botón
-              },
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Stack(children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/board.jpg"),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          DefaultTabController(
+            initialIndex: 1,
+            length: 2,
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Color.fromRGBO(49, 45, 45, 1),
+                title: GestureDetector(
+                  onTap: () {
+                    GoRouter.of(context).push('/');
+                  },
+                  child: Text(
+                    'ChessHub',
+                    style: TextStyle(
+                        fontFamily: 'Oswald',
+                        color: Color.fromRGBO(255, 255, 255, 1)),
                   ),
                 ),
-              ),
-              child: Text('Piezas',
-                  style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para el segundo botón
-              },
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
+                bottom: const TabBar(
+                  labelColor: Color.fromRGBO(255, 136, 0, 1),
+                  indicatorColor: Color.fromRGBO(255, 136, 0, 1),
+                  overlayColor: MaterialStatePropertyAll(Colors.transparent),
+                  tabs: <Widget>[
+                    Tab(
+                      icon: Icon(Icons.brush),
+                    ),
+                    Tab(
+                      icon: Icon(Icons.emoji_emotions),
+                    ),
+                  ],
                 ),
               ),
-              child: Text('Reacciones',
-                  style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para el tercer botón
-              },
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
+              body: TabBarView(
+                children: <Widget>[
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    color: Color.fromRGBO(49, 45, 45, 1),
+                    margin: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: List.generate(
+                            15,
+                            (index) => _buildPieceRow(index + 1),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              child: Text('LO QUE SEA',
-                  style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
-            ),
-            SizedBox(height: 300), // Añade espacio antes del botón 'Back'
-            ElevatedButton(
-              onPressed: () => GoRouter.of(context).go('/'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromRGBO(255, 136, 0, 1)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    color: Color.fromRGBO(49, 45, 45, 1),
+                    margin: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: List.generate(
+                            15,
+                            (index) => _buildEmoteRow(index + 1),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-              child: const Text('Back',
-                  style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
             ),
-            SizedBox(height: 20), // Añade espacio después del botón 'Back'
-            ElevatedButton(
-              onPressed: () => GoRouter.of(context).go('/'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromRGBO(255, 136, 0, 1)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                ),
-              ),
-              child: const Text('GUARDAR',
-                  style: TextStyle(color: Color.fromRGBO(49, 45, 45, 1))),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ]));
   }
+}
+
+Widget _buildPieceRow(int rewardNumber) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 5.0),
+    padding: EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+      border: Border.all(),
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          'Set $rewardNumber',
+          style: TextStyle(fontSize: 32.0),
+        ),
+        /*Image.asset(
+          'assets/images/recompensa_$rewardNumber.jpg', // Asegúrate de tener imágenes llamadas recompensa_1.jpg, recompensa_2.jpg, etc., en la carpeta assets/images/
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),*/
+      ],
+    ),
+  );
+}
+
+Widget _buildEmoteRow(int rewardNumber) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 5.0),
+    padding: EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+      border: Border.all(),
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          'Emote $rewardNumber',
+          style: TextStyle(fontSize: 32.0),
+        ),
+        /*Image.asset(
+          'assets/images/recompensa_$rewardNumber.jpg', // Asegúrate de tener imágenes llamadas recompensa_1.jpg, recompensa_2.jpg, etc., en la carpeta assets/images/
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),*/
+      ],
+    ),
+  );
 }
