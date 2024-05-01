3458e6c12b00061c416988dbcc099b3f4566993a
jorgejaimem8 <845067@unizar.es>
2024-05-01 20:47:48 +0200

    Ranking ya va accediendo a la base de datos

--- lib/ranking/ranking_screen_bullet.dart
+++ lib/ranking/ranking_screen_bullet.dart
@@ -1,12 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import '../style/header.dart';
+import 'package:http/http.dart' as http;
 
 class User {
-  final String name;
-  final int points;
-  final int puesto;
+  final int id;
+  final String nombre;
+  final int elo;
+  
 
-  User({required this.name, required this.points, required this.puesto});
+  User({required this.id, required this.nombre, required this.elo});
+
+  // Constructor factory para crear un objeto User desde un mapa JSON
+  User.fromJson(Map<String, dynamic> json) : 
+      id = json['id'] as int,
+      nombre = json['nombre'] as String,
+      elo = json['elo'] as int;
 }
 
 class RankingScreenBullet extends StatefulWidget {
@@ -15,71 +23,73 @@ class RankingScreenBullet extends StatefulWidget {
 }
 
 class _RankingScreenStateBullet extends State<RankingScreenBullet> {
-  final List<User> users = [
-    User(name: 'elrey900', points: 150, puesto: 1),
-    User(name: 'gambito18', points: 120, puesto: 2),
-    // Añade más usuarios aquí...
-    User(name: 'elxokas', points: 117, puesto: 3),
-    User(name: 'user1231', points: 113, puesto: 4),
-    User(name: 'bot1', points: 100, puesto: 5),
-    User(name: 'Pablo34', points: 96, puesto: 6),
-    User(name: 'Bot de Nunu', points: 92, puesto: 7),
-    User(name: 'jose', points: 87, puesto: 8),
-    User(name: '8325235a', points: 83, puesto: 9),
-    User(name: 'alberto', points: 81, puesto: 10),
-    User(name: 'cr7', points: 79, puesto: 11),
-
-  ];
+  List<User> users = [];
 
   @override
-  Widget build(BuildContext context) {
-    // Ordena la lista de usuarios por puntos en orden descendente
-    users.sort((a, b) => b.points.compareTo(a.points));
-
-    // Encuentra la posición del usuario actual en la lista
-    int userIndex = users.indexWhere((user) => user.name == 'Pablo34');
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Ranking BULLET',
-          style: TextStyle(color: Colors.white,fontFamily: 'Oswald'),
-        ),
-        backgroundColor: Color.fromRGBO(49,45,45,1),
-      ),
-      backgroundColor: Colors.black,
-      body: Container(
-        color: Color.fromRGBO(49, 45, 45, 1),
-        child: ListView.builder(
-          itemCount: 51,
-          itemBuilder: (context, index) {
-            // Calcula el índice real en la lista de usuarios
-            int realIndex = index - 25 + userIndex;
-
-            // Verifica si el índice está dentro de los límites de la lista de usuarios
-            if (realIndex >= 0 && realIndex < users.length) {
-              User user = users[realIndex];
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                child: Card(
-                  elevation: 3,
-                  child: ListTile(
-                    title: Text(
-                      '${user.puesto}. ${user.name}   ${user.points} pts',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), 
-                    ),
-                    // Personaliza el estilo del usuario actual
-                    tileColor: realIndex == userIndex ? Colors.orange[200] : null,
-                  ),
-                ),
-              );
-            } else {
-              // Muestra un contenedor vacío para los elementos que están fuera de la lista de usuarios
-              return SizedBox.shrink();
-            }
-          },
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    fetchLeaderBoard();
   }
+
+  Future<void> fetchLeaderBoard() async {
+    final url = Uri.parse('http://192.168.1.138:3001/users/ranking/bullet');
+    final response = await http.get(url);
+    if(response.statusCode == 200){
+      final userMap = jsonDecode(response.body) as List<dynamic>;
+      List<User> userList = [];
+      userMap.forEach((userData) {
+        userList.add(User.fromJson(userData as Map<String, dynamic>));
+      });
+      // Ordena la lista de usuarios por puntos en orden descendente
+      userList.sort((a, b) => b.elo.compareTo(a.elo));
+      setState(() {
+        users = userList;
+      });
+    } else {
+      throw Exception('Failed to load leaderboard');
+    }
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(
+        'Ranking BULLET',
+        style: TextStyle(color: Colors.white, fontFamily: 'Oswald'),
+      ),
+      backgroundColor: Color.fromRGBO(49, 45, 45, 1),
+    ),
+    backgroundColor: Colors.black,
+    body: Container(
+      color: Color.fromRGBO(49, 45, 45, 1),
+      child: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          User user = users[index];
+          // Define el color de fondo de la caja
+          Color tileColor = Colors.transparent;
+          if (user.nombre == 'calvera') {
+            tileColor = Colors.orange[200]!;
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: Card(
+              elevation: 3,
+              child: ListTile(
+                title: Text(
+                  '${index + 1}.\t ${user.nombre}.\t${user.elo} pts',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), 
+                ),
+                // Establece el color de fondo de la caja
+                tileColor: tileColor,
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
+
 }
